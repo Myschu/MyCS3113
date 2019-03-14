e5eee60652795948b3bd8215c5081acb6db2c7a8
Myschu <40799597+Myschu@users.noreply.github.com>
2019-03-13 21:00:57 -0400

    Add files via upload

---
+++ HW1/NYUCodeBase/main.cpp
@@ -0,0 +1,159 @@
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+#include <SDL.h>
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "ShaderProgram.h"
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include <iostream>
+
+#ifdef _WINDOWS
+#define RESOURCE_FOLDER ""
+#else
+#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+#endif
+
+SDL_Window* displayWindow;
+
+GLuint LoadTexture(const char *filepath) {
+	int w, h, comp;
+	unsigned char* image = stbi_load(filepath, &w, &h, &comp, STBI_rgb_alpha);
+
+	if (image == NULL) {
+		std::cout << "Unable to load image. Make sure the file path is correct\n";
+		assert(false);
+	}
+
+	GLuint retTexture;
+	glGenTextures(1, &retTexture);
+	glBindTexture(GL_TEXTURE_2D, retTexture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	stbi_image_free(image);
+	return retTexture;
+}
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+	SDL_GL_MakeCurrent(displayWindow, context);
+
+#ifdef _WINDOWS
+	glewInit();
+#endif
+
+	glViewport(0, 0, 640, 360);
+
+	ShaderProgram program;
+	program.Load(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+
+	glm::mat4 projectionMatrix = glm::mat4(1.0f);
+	glm::mat4 modelMatrix = glm::mat4(1.0f);
+	glm::mat4 viewMatrix = glm::mat4(1.0f);
+
+	projectionMatrix = glm::ortho(-1.777f, 1.777f, -1.0f, 1.0f, -1.0f, 1.0f);
+
+	glUseProgram(program.programID);
+
+	SDL_Event event;
+	bool done = false;
+	while (!done) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+				done = true;
+			}
+		}
+
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		program.SetModelMatrix(modelMatrix);
+		program.SetProjectionMatrix(projectionMatrix);
+		program.SetViewMatrix(viewMatrix);
+
+		GLuint boxTexture = LoadTexture(RESOURCE_FOLDER"box.png");
+
+		float vertices[] = { 0.15f, -0.1f, -0.15f, -0.1f, -0.15f, -0.4f, -0.15f, -0.4f, 0.15f, -0.4f, 0.15f, -0.1f };
+
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+		glEnableVertexAttribArray(program.positionAttribute);
+
+		float texCoords[] = { 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f };
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glDisableVertexAttribArray(program.positionAttribute);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+
+		GLuint box2Texture = LoadTexture(RESOURCE_FOLDER"boxAlt.png");
+		program.SetModelMatrix(modelMatrix);
+
+		float vertices2[] = { 0.7f, -0.1f, 0.4f, -0.1f, 0.4f, -0.4f, 0.4f, -0.4f, 0.7f, -0.4f, 0.7f, -0.1f };
+
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices2);
+		glEnableVertexAttribArray(program.positionAttribute);
+
+		float texCoords2[] = { 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f };
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords2);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glDisableVertexAttribArray(program.positionAttribute);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+
+		GLuint bgTexture = LoadTexture(RESOURCE_FOLDER"grassMid.png");
+		program.SetModelMatrix(modelMatrix);
+
+		float vertices3[] = { 1.777f, -0.4f, -1.777f, -0.4f, -1.777f, -1.0f, -1.777f, -1.0f, 1.777f, -1.0f, 1.777f, -0.4f };
+
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices3);
+		glEnableVertexAttribArray(program.positionAttribute);
+
+		float texCoords3[] = { 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f };
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords3);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glDisableVertexAttribArray(program.positionAttribute);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+
+		GLuint box3Texture = LoadTexture(RESOURCE_FOLDER"boxItem.png");
+		program.SetModelMatrix(modelMatrix);
+
+		float vertices4[] = { -1.0f, 0.75f, -1.25f, 0.75f, -1.25f, 0.5f, -1.25f, 0.5f, -1.0f, 0.5f, -1.0f, 0.75f };
+
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices4);
+		glEnableVertexAttribArray(program.positionAttribute);
+
+		float texCoords4[] = { 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f };
+		glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords4);
+		glEnableVertexAttribArray(program.texCoordAttribute);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glDisableVertexAttribArray(program.positionAttribute);
+		glDisableVertexAttribArray(program.texCoordAttribute);
+
+		SDL_GL_SwapWindow(displayWindow);
+	}
+
+	SDL_Quit();
+	return 0;
+}
